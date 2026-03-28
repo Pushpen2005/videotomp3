1fa99fcc7f460785311ca91a753dd23fa0c2e576
PUSHPEN TIWARI <pushpentiwari@Mac.bbrouter>
2026-03-28 17:44:09 +0530

    fix cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,11 +5,15 @@ import uploadRouter from './Routes/upload.routes.js';
 const app = express();
 app.use(
   cors({
-    origin: "https://videotomp3-gwji.vercel.app",
-    methods: ["GET", "POST"],
+    origin: [
+      "https://videotomp3-gwji.vercel.app",
+      "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', uploadRouter);
 
